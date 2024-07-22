45afae43f288fde069648cbd1f5dc246a40e46ec
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-07-22 16:39:09 +0330

    Update deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,20 +1,19 @@
-import { ethers, upgrades } from 'hardhat';
-import type { ContractFactory } from 'ethers';
+import { ethers } from 'hardhat';
 
 async function main() {
     console.log('[ 👾 ] Initializing...');
-    console.log(`[ 👾 ] Deploying to chain: ${(await ethers.provider.getNetwork()).name}`);
+    // console.log(`[ 👾 ] Deploying to chain: ${(await ethers.provider.getNetwork()).name}`);
     let heartBeat = 120;
     const droplinkedWallet = "0x47a8378243f702143775a0AD75DD629935DA8AFf";
     const droplinkedFee = 100;
     console.log('[ 👾 ] Droplinked fee is set to 100');
     console.log(`[ 👾 ] Starting deployment...`);
     const DropShopDeployer = await ethers.getContractFactory('DropShopDeployer');
-    const deployer = await upgrades.deployProxy(DropShopDeployer as unknown as ContractFactory, [heartBeat, droplinkedWallet, droplinkedFee], { initializer: 'initialize' });
-    console.log('[ ✅ ] Deployer deployed to: ', await deployer.getAddress());
+    const deployer = await DropShopDeployer.deploy(heartBeat, droplinkedWallet, droplinkedFee);
+    console.log('[ ✅ ] Deployer deployed to: ', deployer.address);
     const ProxyPayer = await ethers.getContractFactory('DroplinkedPaymentProxy');
-    const proxyPayer = await upgrades.deployProxy(ProxyPayer as unknown as ContractFactory, [heartBeat], { initializer: 'initialize' });
-    console.log('[ ✅ ] ProxyPayer deployed to: ', await proxyPayer.getAddress());
+    const proxyPayer = await ProxyPayer.deploy(heartBeat);
+    console.log('[ ✅ ] ProxyPayer deployed to: ', proxyPayer.address);
 }
 
 main()
